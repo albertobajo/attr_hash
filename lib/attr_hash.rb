b17d5912c96e91560e2fec6b93e90379adfa0c9f
Alberto Bajo <albertobajo@gmail.com>
2012-07-05 18:47:43 +0200

    respond_to? implemented.

--- lib/attr_hash.rb
+++ lib/attr_hash.rb
@@ -4,6 +4,11 @@ require "attr_hash/version"
 module AttrHash
   extend ActiveSupport::Concern
   
+  def respond_to?(method, include_private = false)
+    self.attr_hash ||= {}
+    self.attr_hash.key? method.to_sym
+  end
+  
   def method_missing(method, *args, &block)
     self.attr_hash ||= {}
     raise NoMethodError unless self.attr_hash.key? method.to_sym
